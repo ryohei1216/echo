94b48d8e34e3de9305af72badd5bfeb2428ad655
ryohei1216 <miyamoto.ryohei1216@gmail.com>
2023-09-06 10:09:18 +0900

    fix: Change how to convert struct to json

--- cmd/gochi/main.go
+++ cmd/gochi/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
 
 	"github.com/go-chi/chi"
@@ -21,11 +23,7 @@ func main() {
 func handler(w http.ResponseWriter, _ *http.Request) {
 	res := Response{Message: "hello"}
 
-	jsonData, err := json.Marshal(res)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		slog.Warn(fmt.Sprintf("encode response: %v", err))
 	}
-
-	w.Write(jsonData)
 }
--- cmd/http/main.go
+++ cmd/http/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
 )
 
@@ -18,15 +20,11 @@ func main() {
 func handler(w http.ResponseWriter, _ *http.Request) {
 	res := Response{Message: "hello"}
 
-	jsonData, err := json.Marshal(res)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
 	w.Header().Set("Content-Type", "application/json")
 
 	w.WriteHeader(http.StatusOK)
 
-	w.Write(jsonData)
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		slog.Warn(fmt.Sprintf("encode response: %v", err))
+	}
 }
